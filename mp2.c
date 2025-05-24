1e2f6ddc2251757f56771a570e7cf489e72fe5d7
Leonard Ang <lkang@up.edu.ph>
2025-05-25 01:16:45 +0800

    Update mp2.c

--- mp2.c
+++ mp2.c
@@ -3,299 +3,273 @@
 #include <string.h>
 
 #define MAX_CHARS 256
-#define MAX_CODE_LEN 256
 
 typedef struct BinaryTreeNode
 {
-    char data;
+    char ch;
     int freq;
-    struct BinaryTreeNode *left;
-    struct BinaryTreeNode *right;
+    struct BinaryTreeNode *left, *right;
+    unsigned char min_char_in_subtree;
 } BinaryTreeNode;
 
 typedef struct
 {
-    BinaryTreeNode *A[MAX_CHARS];
-    int heap_size;
+    BinaryTreeNode **data;
+    int size;
+    int capacity;
 } PriorityQueue;
 
-#define PARENT(i) (((i) - 1) / 2)
-#define LEFT(i) (2 * (i) + 1)
-#define RIGHT(i) (2 * (i) + 2)
+typedef struct
+{
+    char ch;
+    char code[MAX_CHARS];
+} HuffmanCode;
 
-void count_frequencies(FILE *inputFile, int freq_map[MAX_CHARS]);
 void PQ_INSERT(PriorityQueue *P, BinaryTreeNode *x);
 BinaryTreeNode *PQ_EXTRACT(PriorityQueue *P);
 void HEAPIFY(PriorityQueue *P, int idx);
 BinaryTreeNode *BUILD_HUFFMAN_TREE(PriorityQueue *P);
-void build_encoding_map(BinaryTreeNode *huffmanTree, char encoding_map[MAX_CHARS][MAX_CODE_LEN]);
-void traverse_huffman_tree(BinaryTreeNode *root, char *current_code, int depth, char encoding_map[MAX_CHARS][MAX_CODE_LEN]);
-
-typedef struct
-{
-    char c;
-    char code[MAX_CODE_LEN];
-} HuffmanCodeEntry;
-
+void swap_nodes(BinaryTreeNode **a, BinaryTreeNode **b);
+int parent(int i);
+int left(int i);
+int right(int i);
+PriorityQueue *create_priority_queue(int capacity);
+BinaryTreeNode *create_node(char ch_val, int freq_val);
+void generate_codes_recursive(BinaryTreeNode *root, char *current_code_str, int depth, HuffmanCode *codes_array, int *codes_count);
 int compare_huffman_codes(const void *a, const void *b);
-void print_huffman_codes(char encoding_map[MAX_CHARS][MAX_CODE_LEN], int freq_map[MAX_CHARS]);
+void free_huffman_tree(BinaryTreeNode *root);
 
-int main()
+void swap_nodes(BinaryTreeNode **a, BinaryTreeNode **b)
 {
-    FILE *inputFile = stdin;
-
-    if (inputFile == NULL)
-    {
-        perror("Error opening input");
-        return 1;
-    }
-
-    int freq_map[MAX_CHARS] = {0};
-        count_frequencies(inputFile, freq_map);
-
-    PriorityQueue P;
-    P.heap_size = 0;
-
-    for (int i = 0; i < MAX_CHARS; i++)
-    {
-        if (freq_map[i] > 0)
-        {
-            BinaryTreeNode *newNode = (BinaryTreeNode *)malloc(sizeof(BinaryTreeNode));
-            if (!newNode)
-            {
-                perror("Failed to allocate memory for node");
-                exit(EXIT_FAILURE);
-            }
-            newNode->data = (char)i;
-            newNode->freq = freq_map[i];
-            newNode->left = NULL;
-            newNode->right = NULL;
-            PQ_INSERT(&P, newNode);
-        }
-    }
-
-    BinaryTreeNode *huffmanTree = BUILD_HUFFMAN_TREE(&P);
-
-    char encoding_map[MAX_CHARS][MAX_CODE_LEN];
-    for (int i = 0; i < MAX_CHARS; i++)
-    {
-        encoding_map[i][0] = '\0';
-    }
-    if (huffmanTree)
-    {
-        build_encoding_map(huffmanTree, encoding_map);
-    }
-
-    print_huffman_codes(encoding_map, freq_map);
-
-    return 0;
+    BinaryTreeNode *tmp = *a;
+    *a = *b;
+    *b = tmp;
 }
 
-void count_frequencies(FILE *inputFile, int freq_map[MAX_CHARS])
+int parent(int i) { return (i - 1) / 2; }
+int left(int i) { return 2 * i + 1; }
+int right(int i) { return 2 * i + 2; }
+
+int is_less_than(BinaryTreeNode *n1, BinaryTreeNode *n2)
 {
-    int c;
-    while ((c = fgetc(inputFile)) != EOF && c != ';')
-    {
-        freq_map[(unsigned char)c]++;
-    }
+    if (n1->freq < n2->freq)
+        return 1;
+    if (n1->freq > n2->freq)
+        return 0;
+
+    return n1->min_char_in_subtree < n2->min_char_in_subtree;
 }
 
 void HEAPIFY(PriorityQueue *P, int idx)
 {
-    int l = LEFT(idx);
-    int r = RIGHT(idx);
+    int l = left(idx);
+    int r = right(idx);
     int smallest = idx;
 
-    if (l < P->heap_size && P->A[l]->freq < P->A[smallest]->freq)
-    {
+    if (l < P->size && is_less_than(P->data[l], P->data[smallest]))
         smallest = l;
-    }
-    if (r < P->heap_size && P->A[r]->freq < P->A[smallest]->freq)
-    {
+    if (r < P->size && is_less_than(P->data[r], P->data[smallest]))
         smallest = r;
-    }
 
     if (smallest != idx)
     {
-        BinaryTreeNode *temp = P->A[idx];
-        P->A[idx] = P->A[smallest];
-        P->A[smallest] = temp;
+        swap_nodes(&P->data[idx], &P->data[smallest]);
         HEAPIFY(P, smallest);
     }
 }
 
 void PQ_INSERT(PriorityQueue *P, BinaryTreeNode *x)
 {
-    if (P->heap_size == MAX_CHARS)
+    if (P->size == P->capacity)
     {
-        return;
+        P->capacity *= 2;
+        P->data = realloc(P->data, P->capacity * sizeof(BinaryTreeNode *));
+        if (!P->data)
+        {
+            perror("Failed to reallocate memory for priority queue");
+            exit(EXIT_FAILURE);
+        }
     }
+    int i = P->size++;
+    P->data[i] = x;
 
-    P->heap_size++;
-    int i = P->heap_size - 1;
-    P->A[i] = x;
-
-    while (i > 0 && P->A[PARENT(i)]->freq > P->A[i]->freq)
+    while (i != 0 && is_less_than(P->data[i], P->data[parent(i)]))
     {
-        BinaryTreeNode *temp = P->A[i];
-        P->A[i] = P->A[PARENT(i)];
-        P->A[PARENT(i)] = temp;
-        i = PARENT(i);
+        swap_nodes(&P->data[i], &P->data[parent(i)]);
+        i = parent(i);
     }
 }
 
 BinaryTreeNode *PQ_EXTRACT(PriorityQueue *P)
 {
-    if (P->heap_size < 1)
-    {
+    if (P->size == 0)
         return NULL;
-    }
-
-    BinaryTreeNode *min_node = P->A[0];
-    P->A[0] = P->A[P->heap_size - 1];
-    P->heap_size--;
-
-    if (P->heap_size > 0)
+    BinaryTreeNode *min_node = P->data[0];
+    P->data[0] = P->data[--P->size];
+    if (P->size > 0)
     {
         HEAPIFY(P, 0);
     }
     return min_node;
 }
 
+PriorityQueue *create_priority_queue(int capacity)
+{
+    PriorityQueue *P = malloc(sizeof(PriorityQueue));
+    if (!P)
+    {
+        perror("Failed to allocate memory for priority queue");
+        exit(EXIT_FAILURE);
+    }
+    P->data = malloc(capacity * sizeof(BinaryTreeNode *));
+    if (!P->data)
+    {
+        perror("Failed to allocate memory for priority queue data");
+        free(P);
+        exit(EXIT_FAILURE);
+    }
+    P->size = 0;
+    P->capacity = capacity;
+    return P;
+}
+
+BinaryTreeNode *create_node(char ch_val, int freq_val)
+{
+    BinaryTreeNode *node = malloc(sizeof(BinaryTreeNode));
+    if (!node)
+    {
+        perror("Failed to allocate memory for node");
+        exit(EXIT_FAILURE);
+    }
+    node->ch = ch_val;
+    node->freq = freq_val;
+    node->min_char_in_subtree = (unsigned char)ch_val;
+    node->left = node->right = NULL;
+    return node;
+}
+
 BinaryTreeNode *BUILD_HUFFMAN_TREE(PriorityQueue *P)
 {
-    if (P->heap_size == 0)
-    {
+    if (P == NULL || P->size == 0)
         return NULL;
-    }
 
-    if (P->heap_size == 1)
+    while (P->size > 1)
     {
-        BinaryTreeNode *single_char_node = PQ_EXTRACT(P);
+        BinaryTreeNode *left_c = PQ_EXTRACT(P);
+        BinaryTreeNode *right_c = PQ_EXTRACT(P);
 
-        BinaryTreeNode *dummy_parent = (BinaryTreeNode *)malloc(sizeof(BinaryTreeNode));
-        if (!dummy_parent)
-        {
-            perror("Failed to allocate memory for dummy parent");
-            exit(EXIT_FAILURE);
-        }
-        dummy_parent->data = 0;
-        dummy_parent->freq = single_char_node->freq;
-        dummy_parent->left = single_char_node;
-        dummy_parent->right = NULL;
-        return dummy_parent;
-    }
+        BinaryTreeNode *parent_node = create_node('\0', left_c->freq + right_c->freq);
 
-    while (P->heap_size > 1)
-    {
-        BinaryTreeNode *left_child = PQ_EXTRACT(P);
-        BinaryTreeNode *right_child = PQ_EXTRACT(P);
+        parent_node->min_char_in_subtree = (left_c->min_char_in_subtree < right_c->min_char_in_subtree) ? left_c->min_char_in_subtree : right_c->min_char_in_subtree;
 
-        BinaryTreeNode *internal_node = (BinaryTreeNode *)malloc(sizeof(BinaryTreeNode));
-        if (!internal_node)
-        {
-            perror("Failed to allocate memory for internal node");
-            exit(EXIT_FAILURE);
-        }
-        internal_node->data = 0;
-        internal_node->freq = left_child->freq + right_child->freq;
-        internal_node->left = left_child;
-        internal_node->right = right_child;
+        parent_node->left = left_c;
+        parent_node->right = right_c;
 
-        PQ_INSERT(P, internal_node);
+        PQ_INSERT(P, parent_node);
     }
     return PQ_EXTRACT(P);
 }
 
-void build_encoding_map(BinaryTreeNode *huffmanTree, char encoding_map[MAX_CHARS][MAX_CODE_LEN])
+void generate_codes_recursive(BinaryTreeNode *root, char *current_code_str, int depth, HuffmanCode *codes_array, int *codes_count)
 {
-    char current_code[MAX_CODE_LEN];
-    traverse_huffman_tree(huffmanTree, current_code, 0, encoding_map);
+    if (!root)
+        return;
+
+    if (!root->left && !root->right)
+    {
+        current_code_str[depth] = '\0';
+        codes_array[*codes_count].ch = root->ch;
+        strcpy(codes_array[*codes_count].code, current_code_str);
+        (*codes_count)++;
+        return;
+    }
+
+    current_code_str[depth] = '0';
+    generate_codes_recursive(root->left, current_code_str, depth + 1, codes_array, codes_count);
+
+    current_code_str[depth] = '1';
+    generate_codes_recursive(root->right, current_code_str, depth + 1, codes_array, codes_count);
 }
 
-void traverse_huffman_tree(BinaryTreeNode *root, char *current_code, int depth, char encoding_map[MAX_CHARS][MAX_CODE_LEN])
+int compare_huffman_codes(const void *a, const void *b)
+{
+    HuffmanCode *hc1 = (HuffmanCode *)a;
+    HuffmanCode *hc2 = (HuffmanCode *)b;
+    return strcmp(hc1->code, hc2->code);
+}
+
+void free_huffman_tree(BinaryTreeNode *root)
 {
     if (root == NULL)
     {
         return;
     }
-
-    if (root->left == NULL && root->right == NULL)
-    {
-        current_code[depth] = '\0';
-        strcpy(encoding_map[(unsigned char)root->data], current_code);
-        return;
-    }
-
-    if (root->left)
-    {
-        current_code[depth] = '0';
-        traverse_huffman_tree(root->left, current_code, depth + 1, encoding_map);
-    }
-
-    if (root->right)
-    {
-        current_code[depth] = '1';
-        traverse_huffman_tree(root->right, current_code, depth + 1, encoding_map);
-    }
+    free_huffman_tree(root->left);
+    free_huffman_tree(root->right);
+    free(root);
 }
 
-int compare_huffman_codes(const void *a, const void *b)
+int main()
 {
-    HuffmanCodeEntry *entryA = (HuffmanCodeEntry *)a;
-    HuffmanCodeEntry *entryB = (HuffmanCodeEntry *)b;
-    int cmp = strcmp(entryA->code, entryB->code);
-    if (cmp == 0)
+    int freq[MAX_CHARS] = {0};
+    int c;
+    int unique_char_count = 0;
+    while ((c = getchar()) != EOF && c != ';')
     {
-        return entryA->c - entryB->c;
-    }
-    return cmp;
-}
-
-void print_huffman_codes(char encoding_map[MAX_CHARS][MAX_CODE_LEN], int freq_map[MAX_CHARS])
-{
-    HuffmanCodeEntry sorted_codes[MAX_CHARS];
-    int num_entries = 0;
-
-    for (int i = 0; i < MAX_CHARS; i++)
-    {
-        if (freq_map[i] > 0)
+        if (freq[(unsigned char)c] == 0)
         {
-            sorted_codes[num_entries].c = (char)i;
-            if (encoding_map[i][0] != '\0')
-            {
-                strcpy(sorted_codes[num_entries].code, encoding_map[i]);
-            }
-            else if (freq_map[i] > 0 && encoding_map[i][0] == '\0')
-            {
-                strcpy(sorted_codes[num_entries].code, "0");
-            }
-            else
-            {
-                strcpy(sorted_codes[num_entries].code, "");
-            }
-            num_entries++;
+            unique_char_count++;
+        }
+        freq[(unsigned char)c]++;
+    }
+
+    if (unique_char_count == 0)
+    {
+        return 0;
+    }
+
+    PriorityQueue *P = create_priority_queue(unique_char_count > 0 ? unique_char_count : 16);
+    for (int i = 0; i < MAX_CHARS; ++i)
+    {
+        if (freq[i] > 0)
+        {
+            PQ_INSERT(P, create_node((char)i, freq[i]));
         }
     }
 
-    if (num_entries == 0)
-        return;
+    BinaryTreeNode *root = BUILD_HUFFMAN_TREE(P);
 
-    qsort(sorted_codes, num_entries, sizeof(HuffmanCodeEntry), compare_huffman_codes);
+    HuffmanCode codes_array[MAX_CHARS];
+    int codes_count = 0;
+    char current_code_str[MAX_CHARS];
 
-    for (int i = 0; i < num_entries; i++)
+    if (root)
     {
-        if (sorted_codes[i].c == '\n')
+        if (unique_char_count == 1)
         {
-            printf(" :: %s\n", sorted_codes[i].code);
-        }
-        else if (sorted_codes[i].c == '\t')
-        {
-            printf("\\t :: %s\n", sorted_codes[i].code);
+            codes_array[0].ch = root->ch;
+            strcpy(codes_array[0].code, "0");
+            codes_count = 1;
         }
         else
         {
-            printf("%c :: %s\n", sorted_codes[i].c, sorted_codes[i].code);
+            generate_codes_recursive(root, current_code_str, 0, codes_array, &codes_count);
         }
     }
+
+    qsort(codes_array, codes_count, sizeof(HuffmanCode), compare_huffman_codes);
+
+    for (int i = 0; i < codes_count; ++i)
+    {
+        printf("%c :: %s\n", codes_array[i].ch, codes_array[i].code);
+    }
+
+    free_huffman_tree(root);
+    if (P)
+    {
+        free(P->data);
+        free(P);
+    }
+
+    return 0;
 }
